9eb4ffd0178856dc18a09a0ff1a86c5de4b45bc8
Zver <Zver@ZVERDVD-PKGJ48T>
2021-10-25 16:57:30 +1000

    Добавьте файлы проекта.

---
+++ ZTYoung101/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTYoung101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char array = 'n';
+            Random r = new Random();
+            while (array == 'n')
+            {
+                int i = r.Next(10);
+
+                Console.WriteLine("Загаданное число N >= 0 & N <= 10");
+                Console.WriteLine("Введите число");
+
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (i == x)
+                    Console.WriteLine("Вы угадали");
+                else
+                    Console.WriteLine("Не угадали");
+
+                Console.WriteLine("Еще хотите попробовайт? (n - да, m - неть)");
+                array = Convert.ToChar(Console.ReadLine());
+            }
+
+        }
+    }
+}
